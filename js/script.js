53e9f7384b827ba54e5139d30143f6caed8498bf
NirvikPurkait <93871471+NirvikPurkait@users.noreply.github.com>
2022-07-23 16:24:45 +0530

    improved navbar
    added circle for small device

--- js/script.js
+++ js/script.js
@@ -4,12 +4,5 @@ const navBar = document.getElementById('nav-bar');
 hamburgerButton.addEventListener('click', displayNavbar);
 
 function displayNavbar() {
-//   if (navBar.style.display == 'flex') {
-//     navBar.style.display = 'none';
-//   }
-//   else navBar.style.display = 'flex';
-
-//   console.log(hamburgerButton);
-  
   navBar.classList.toggle('open-nav-bar')
 }
